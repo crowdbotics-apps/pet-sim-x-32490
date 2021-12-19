ec2eec4a4a7beace28ce27dbd8f0bec60f291c44
Crowdbotics <team@crowdbotics.com>
2021-12-19 18:35:21 +0000

    Update models

--- backend/pet_sim_x_32490/wsgi.py
+++ backend/pet_sim_x_32490/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_sim_x_32490.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pet_sim_x_32490.settings")
 
 application = get_wsgi_application()
